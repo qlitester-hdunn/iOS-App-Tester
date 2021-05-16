628779b3718fa4e3d1842bbbe1426304718e7e5c
hdunn <qlihodunn@gmail.com>
2021-05-16 11:07:35 -0600

    assets

---
+++ iOS-App-Tester/Scenes/Main.swift
@@ -0,0 +1,22 @@
+//
+//  Main.swift
+//  iOS-App-Tester
+//
+//  Created by Tester on 5/16/21.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct Main_Previews: PreviewProvider {
+    static var previews: some View {
+        Main()
+    }
+}
