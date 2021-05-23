55f07daed4bde8284cd7b176aeedc4875a928388
hdunn <qlihodunn@gmail.com>
2021-05-22 21:33:34 -0600

    dir struct

--- iOS-App-Tester/Components/Buttons/Main/MainButtonFactory.swift
+++ iOS-App-Tester/Components/Buttons/Main/MainButtonFactory.swift
@@ -14,19 +14,21 @@ class MainButtonFactory {
     static let numBtns = CGFloat(10)
     static let fontSz = CGFloat((((deviceScreenHeight * 0.30)/MainButtonFactory.numBtns) > 21.0) ? 21.0 : 16.0)
     static let textMaxHt = CGFloat(((deviceScreenHeight * 0.35)/MainButtonFactory.numBtns) > fontSz ? fontSz * 1.2 : fontSz * 1.03)
-    
-    static func newButton(text: MainButtonName, action: @escaping () -> Void) -> some View {
+    static let btnWidth = deviceScreenWidth * 0.80
+    static func newButton(text: MainButtonName, destination: String, action: @escaping () -> Void) -> some View {
         return
-            NavigationLink(destination: TypeText()){
-                ZStack {
-                    RoundedRectangle(cornerRadius: 5)
-                        .background(Color(.lightGray))
-                        .frame(maxWidth: deviceScreenWidth * 0.80, maxHeight:(deviceScreenHeight * 0.60)/MainButtonFactory.numBtns)
-                        
-                    newText(text.rawValue)
+            (
+                NavigationLink(destination: TypeText()){
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20, style: .continuous)
+                            .background(Color(.lightGray))
+                            .frame(maxWidth: btnWidth, maxHeight: textMaxHt * 3)
+                            
+                        //newText(text.rawValue)
+                    }
+                }.background(Color(.lightGray))
                 
-                }
-            }
+            )
     }
     
     static func newText(_ text: String) -> some View {
@@ -35,8 +37,7 @@ class MainButtonFactory {
             .font(Font.custom("Helvetica Neue", size: fontSz))
             .padding(4)
             .foregroundColor(Color("BtnText"))
-            .background(Color(.lightGray))
-            .frame(maxWidth: .infinity, maxHeight: textMaxHt)
+            .frame(maxWidth:.infinity, maxHeight: textMaxHt)
         
         return text
     }
