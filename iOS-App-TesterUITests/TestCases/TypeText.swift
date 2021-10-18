3b17fb4099405c9b23faf39d66a29b7d82afbbfe
Dorian Bizgan <dbizgan@MBP-06.local>
2021-10-18 15:45:31 -0700

    Atrocity: Broken waitForDisplay

--- iOS-App-TesterUITests/TestCases/TypeText.swift
+++ iOS-App-TesterUITests/TestCases/TypeText.swift
@@ -7,61 +7,73 @@
 
 import XCTest
 
-extension XCTWaiter.Result {
-    
-    func bool(isInverted: Bool = false, isOrdered: Bool = false) -> Bool {
-        switch (self) {
-        case XCTWaiter.Result.completed:
-            return true
-            
-        case XCTWaiter.Result.timedOut:
-            return false
-            
-        case XCTWaiter.Result.incorrectOrder:
-            return (isOrdered) ? false : true
-            
-        case XCTWaiter.Result.invertedFulfillment:
-            return (isInverted) ? false : true
-            
-        case XCTWaiter.Result.interrupted:
-            return false
-        default:
-            return false
-        }
-    }
-}
+//extension XCTWaiter.Result {
+//
+//    func bool(isInverted: Bool = false, isOrdered: Bool = false) -> Bool {
+//        switch (self) {
+//        case XCTWaiter.Result.completed:
+//            return true
+//
+//        case XCTWaiter.Result.timedOut:
+//            return false
+//
+//        case XCTWaiter.Result.incorrectOrder:
+//            return (isOrdered) ? false : true
+//
+//        case XCTWaiter.Result.invertedFulfillment:
+//            return (isInverted) ? false : true
+//
+//        case XCTWaiter.Result.interrupted:
+//            return false
+//        default:
+//            return false
+//        }
+//    }
+//}
 
 class TypeText: iOS_App_TesterUITests {
     static var timeout: TimeInterval = 1.0
-    let idetifierStrings = ["Hello World!"]
-    func expectation(for condition: @autoclosure @escaping () -> Bool, description: String = "") -> XCTestExpectation {
-        let predicate = NSPredicate { _, _ in
-            return condition()
-        }
-        
-        return XCTNSPredicateExpectation(predicate: predicate, object: nil)
-    }
+    let identifierStrings = ["Hello,World"]
+//    func expectation(for condition: @autoclosure @escaping () -> Bool, description: String = "") -> XCTestExpectation {
+//        let predicate = NSPredicate { _, _ in
+//            return condition()
+//        }
+//
+//        return XCTNSPredicateExpectation(predicate: predicate, object: nil)
+//    }
     
-    @discardableResult
-    func wait(for condition: @autoclosure @escaping () -> Bool, description: String = "", timeout: TimeInterval = timeout) -> Bool {
-        guard !condition() else { return false }
-        let expect = expectation(for: condition(), description: description)
-        return XCTWaiter.wait(for: [expect], timeout: timeout).bool()
-    }
+//    @discardableResult
+//    func wait(for condition: @autoclosure @escaping () -> Bool, description: String = "", timeout: TimeInterval = timeout) -> Bool {
+//        guard !condition() else { return false }
+//        let expect = expectation(for: condition(), description: description)
+//        return XCTWaiter.wait(for: [expect], timeout: timeout).bool()
+//    }
+    
     
     func testExample() {
         
         let headingText: XCUIElement = self.app.staticTexts["Type Text"].firstMatch
-        let textView: XCUIElement = self.app.textViews["Hello, World!"].firstMatch
+        let textView: XCUIElement = self.app.textViews["Hello,World"].firstMatch
         let labelTypeText: XCUIElement = self.app.staticTexts["Type Text"].firstMatch
+        let backButton: XCUIElement = self.app.images["arrow.left"].firstMatch
         func navigateToTextType() -> Void {
             
             let condition: () -> Bool = {
-                self.app.buttons["Type Text"].firstMatch.exists
+                return self.app.buttons["Type Text"].firstMatch.exists
+                
             }
-            
-            wait(for: condition(), description: "Wait for Type Text button", timeout: TypeText.timeout)
+        
+//            wait(for: condition(), description: "Wait for Type Text button", timeout: TypeText.timeout)
             self.app.buttons["Type Text"].firstMatch.tap()
+        }
+        
+        func verifyBackButton(){
+            backButton.tap()
+            XCTAssert(self.app.buttons["Type Text"].firstMatch.exists)
+        }
+        
+        func verifyHeadingText(){
+            XCTAssert(headingText.label == "Type Text")
         }
         
         func verifyTextViewLabel(){
@@ -71,25 +83,52 @@ class TypeText: iOS_App_TesterUITests {
         func verifyTextView(){
             XCTAssert(textView.exists)
         }
+//        func waitFor(for identifiers: [String]){
+//            identifiers.forEach({
+//                (id) in
+//                print("\(id)")
+//                wait(for:  self.app.staticTexts[id].firstMatch.exists, timeout: TypeText.timeout)
+//            })
+//        }
         
         func waitForDisplay(for identifiers: [String], type: XCUIElement.ElementType? = nil) -> Void {
             identifiers.forEach({
-                (id) in
-                let type = type ?? XCUIElement.ElementType.staticText
+                (id) in type ?? 
+                print(String(describing: "\(id)"))
                     switch (type) {
                     case XCUIElement.ElementType.staticText:
-                        wait(for:  self.app.staticTexts[id].firstMatch.exists )
+                        XCTAssert(self.app.staticTexts[id].firstMatch.waitForExistence(timeout: TypeText.timeout))
                     case XCUIElement.ElementType.textField:
-                        wait(for:  self.app.textFields[id].firstMatch.exists )
+                        XCTAssert(self.app.textFields[id].firstMatch.waitForExistence(timeout: TypeText.timeout))
                     default:
                         print(String(describing: "Element '\(id)' does not exist"))
                     }
             })
+            
+            print("HELLO WORLD")
         }
         
+//        func waitForDisplay(for identifiers: [String], type: XCUIElement.ElementType? = nil) -> Void {
+//            identifiers.forEach({
+//                (id) in
+//                let type = type ?? XCUIElement.ElementType.staticText
+//                    switch (type) {
+//                    case XCUIElement.ElementType.staticText:
+//                        wait(for:  self.app.staticTexts[id].firstMatch.exists )
+//                    case XCUIElement.ElementType.textField:
+//                        wait(for:  self.app.textFields[id].firstMatch.exists )
+//                    default:
+//                        print(String(describing: "Element '\(id)' does not exist"))
+//                    }
+//            })
+//        }
+        
         navigateToTextType()
-        waitForDisplay(for: self.idetifierStrings, type: XCUIElement.ElementType.textView)
+        waitForDisplay(for: identifierStrings, type)
+        verifyHeadingText()
         verifyTextViewLabel()
         verifyTextView()
+        verifyBackButton()
+    
     }
 }
